d1e6efb0027c0fee98b14e2f14a16a05e6fcc827
EliGarcia <garciaelisha93@yahoo.com>
2017-08-18 15:30:55 -0500

    adding psudo code to each line to solidify what i have learned

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 
+//make an array for my todo items
 let myData = [
+  //when making the todoItems i give them an id of a number so i can work with each item later
   {'todoItem':' give bailey a highfive!', 'done':false, 'id':1},
   {'todoItem':' call mom', 'done':false, 'id':2},
   {'todoItem':' gossip', 'done':false, 'id':3},
@@ -19,15 +21,21 @@ let myData = [
   {'todoItem':' turn myself into a pickle', 'done':false, 'id':5},
   {'todoItem':' wake up morty', 'done':false, 'id':6}
 ];
-
+//getting the root page and returning a req and a res
 app.get('/', function (req, res) {
+  //after getting the root page i then render my mustache page to it and assigning 'mydata' to data. now i can use {{data}} in my mustache that links with app.js
 res.render('todo.mustache', {data: myData})
 });
 
+// i am now posting on my root ('/')
 app.post('/', function (req, res) {
+  // here i am setting newTodoItem to the value of the newItem input in the req
   const newTodoItem = req.body.newItem;
+  //my maxId will now equal the length of my array
   let maxId = myData.length;
+  // now newItem is the new object that i will put into myData and it then take the id number an assigns one the is +1 higher
   let newItem = {'todoItem': newTodoItem, 'done':false, 'id': maxId + 1}
+  //then i push newItem into mtData array
   myData.push(newItem);
   res.redirect('/')
 })
